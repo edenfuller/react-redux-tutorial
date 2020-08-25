61228d9abbac40d05db0f80acf46b55a9e8f4b95
Eden Fuller <evan.fuller@ingka.ikea.com>
2020-08-25 09:39:48 -0400

    Reorder index.js imports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import { Provider } from "react-redux";
-import rootReducer from "./reducers/index";
-
 import { createStore } from "redux";
+
+import rootReducer from "./reducers/index";
 
 import App from "./App";
 const store = createStore(
