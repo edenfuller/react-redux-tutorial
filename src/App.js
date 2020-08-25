351f1d4e9d62ebaf65bc19549814095b15743a1b
Eden Fuller <evan.fuller@ingka.ikea.com>
2020-08-25 12:05:08 -0400

    Add Checkout to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Inventory from "./components/Inventory";
+import Checkout from "./components/Checkout";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Inventory />
+      <Checkout />
     </div>
   );
 }
