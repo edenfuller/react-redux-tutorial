da20516a3c2a9cc656f1deb917994b9909c4cf41
Eden Fuller <evan.fuller@ingka.ikea.com>
2020-08-25 11:15:45 -0400

    Map state to props in AddRemoveCart

--- src/components/AddRemoveCart.jsx
+++ src/components/AddRemoveCart.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+import { connect } from "react-redux";
 
 const AddRemoveCart = (p) => {
   return (
     <div className="add-remove">
-      <p className="cart-quantity">{`in cart: `}</p>
-      <p className="stock-quantity">{`in stock: `}</p>
+      <p className="cart-quantity">{`in cart: ${p.cart}`}</p>
+      <p className="stock-quantity">{`in stock: ${p.inventory}`}</p>
       <button className="remove">-</button>
       <button className="add">+</button>
     </div>
   );
 };
 
-export default AddRemoveCart;
+const mapStateToProps = (state, p) => {
+  return { cart: state.cart[p.name], inventory: state.inventory[p.name] };
+};
+// export default VisibilityFilters;
+const Container = connect(mapStateToProps)(AddRemoveCart);
+export default Container;
