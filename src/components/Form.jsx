81f0f3efbfd280e7a9d0f830f4b4ad46d4129889
Eden Fuller <evan.fuller@ingka.ikea.com>
2021-02-18 13:45:05 -0500

    Render separate React component trees with shared Redux logic

---
+++ src/components/Form.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import InputField from "@ingka/input-field";
+
+const Form = (p) => {
+  return <div id="Form">
+    <InputField />
+  </div>;
+}
+export const Form;
--- src/index.js
+++ src/index.js
@@ -6,15 +6,27 @@ import { createStore } from "redux";
 import rootReducer from "./reducers/index";
 
 import App from "./App";
+import Checkout from "./components/Checkout";
+
 const store = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 const rootElement = document.getElementById("root");
+console.log(rootElement);
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   rootElement
+);
+
+const checkoutElement = document.getElementById("checkout-render");
+console.log(checkoutElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <Checkout />
+  </Provider>,
+  checkoutElement
 );
